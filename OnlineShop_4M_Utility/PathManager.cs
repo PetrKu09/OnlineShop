3b7ae9372176ee305a8dbeaa539ea9b8b18e6dc7
PetrKu09 <76242772+PetrKu09@users.noreply.github.com>
2024-06-28 13:10:53 +0300

    2
    2

---
+++ OnlineShop_4M_Utility/PathManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OnlineShop_4M_Utility
+{
+	public static class PathManager
+	{
+		public static string ImagePath = @"/images/product/";
+        public static string TemplatesPath = @"/templates/";
+        public static string SessionCart = "ShoppingCartSession";
+        public static string SessionInquiryId = "InquirySession";
+
+        public const string AdminRole = "Admin";
+		public const string CustomerRole = "Customer";
+
+		public const string CategoryName = "Category";
+		public const string CompanyName = "Company";
+	}
+}
+
